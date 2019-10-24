19d8a648f2c8d11bf2f67df9437925a0b6ce7edc
Gløer Olav Langslet <gloer@langslet.com>
2019-10-24 10:10:21 +0200

    Car Animation

---
+++ tesla-big/index.js
@@ -0,0 +1,6 @@
+const bil = document.querySelector("#bil");
+const btnWhite = document.querySelector("#btnWhite");
+
+btnWhite.onclick = () => {
+    bil.style.backgroundImage =  `url("./img/blue-car.png")`; 
+}
